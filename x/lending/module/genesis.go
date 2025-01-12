34591176805e9236e6a64147d3275f86a06ff6e8
keithsue <sufay28301@gmail.com>
2025-01-12 23:43:02 +0800

    improve genesis handling

--- x/lending/module/genesis.go
+++ x/lending/module/genesis.go
@@ -10,11 +10,19 @@ import (
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
+	k.SetParams(ctx, genState.Params)
+
+	// set pools
+	for _, pool := range genState.Pools {
+		k.SetPool(ctx, *pool)
+	}
 }
 
 // ExportGenesis returns the module's exported genesis
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
+
+	genesis.Pools = k.GetAllPools(ctx)
 
 	// this line is used by starport scaffolding # genesis/module/export
 
