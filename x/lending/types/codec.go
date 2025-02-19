ab4f78b0ba8d5db17fac991455d3cf7e984e1bfb
keithsue <sufay28301@gmail.com>
2025-02-19 18:25:00 +0800

    register lending cdc

--- x/lending/types/codec.go
+++ x/lending/types/codec.go
@@ -3,14 +3,36 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgCreatePool{}, "lending/MsgCreatePool", nil)
+	cdc.RegisterConcrete(&MsgAddLiquidity{}, "lending/MsgAddLiquidity", nil)
+	cdc.RegisterConcrete(&MsgRemoveLiquidity{}, "lending/MsgRemoveLiquidity", nil)
+	cdc.RegisterConcrete(&MsgApply{}, "lending/MsgApply", nil)
+	cdc.RegisterConcrete(&MsgApprove{}, "lending/MsgApprove", nil)
+	cdc.RegisterConcrete(&MsgRedeem{}, "lending/MsgRedeem", nil)
+	cdc.RegisterConcrete(&MsgRepay{}, "lending/MsgRepay", nil)
+	cdc.RegisterConcrete(&MsgClose{}, "lending/MsgClose", nil)
+	cdc.RegisterConcrete(&MsgSubmitRepaymentAdaptorSignature{}, "lending/MsgSubmitRepaymentAdaptorSignature", nil)
+	cdc.RegisterConcrete(&MsgSubmitLiquidationCetSignatures{}, "lending/MsgSubmitLiquidationCetSignatures", nil)
 	// this line is used by starport scaffolding # 2
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgCreatePool{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgAddLiquidity{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgRemoveLiquidity{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgApply{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgApprove{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgRedeem{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgRepay{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgClose{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgSubmitRepaymentAdaptorSignature{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgSubmitLiquidationCetSignatures{})
+
 	// this line is used by starport scaffolding # 3
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
