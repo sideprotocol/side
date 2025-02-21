bfc9ff3dd36b3f77323f831359de88b99b44d01c
lazyluis <ping@side.one>
2025-02-21 14:15:15 +0800

    add options for testing

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -20,6 +20,15 @@ fi
 if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
   rm -rf $HOME/.shuttler*
+  echo "Create lending pool"
+  $BINARY tx lending create-pool usdc uusdc --from validator --keyring-backend $KEYRING  --fees 100uside --gas auto --chain-id $CHAINID --yes
+  sleep 6
+  echo "Add liquidity"
+  $BINARY tx lending add-liquidity usdc 1000000000uusdc --from validator --keyring-backend $KEYRING  --fees 1000uside --gas auto --chain-id $CHAINID --yes
+  sleep 6
+  echo "Initiail Oracle Prices"
+  $BINARY tx lending submit-price 50000 --from validator --keyring-backend $KEYRING --fees 1000uside --gas auto --chain-id $CHAINID -y
+  sleep 6
 
   # Init shuttler home
   for i in $(seq 1 $N); do
@@ -33,65 +42,78 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
     $BINARY tx bank send validator $($TSS_BIN --home "$HOME/.shuttler$i" address | grep tb1) 10000000uside --chain-id $CHAINID --keyring-backend $KEYRING --yes --fees 2000uside
     sleep 6
   done
+
 fi
 
-declare -a TSS_PARTICIPANTS
-for i in $(seq 1 $N); do
-    key=$(jq .pub_key.value $HOME/.shuttler$i/priv_validator_key.json)
-    TSS_PARTICIPANTS+=($key)
-done
+AVAILABLE_ORACLE=$($BINARY q dlc oracles 3 --output json | jq -r ".oracles | length")
 
-PARTICIPANTS=$(printf "%s," ${TSS_PARTICIPANTS[@]})
+if (($AVAILABLE_ORACLE > 0)); then
+	echo "Create a new Oracle and Agency? [y/n]"
+	read -r overwrite
+else
+	overwrite="y"
+fi
 
-ORACLE_PROPOSAL="{
- \"messages\": [
- {
-  \"@type\": \"/side.dlc.MsgCreateOracle\",
-  \"authority\": \"side10d07y265gmmuvt4z0w9aw880jnsr700jwrwlg5\",
-  \"participants\": [ ${PARTICIPANTS%?} ],
-  \"threshold\": $T
- }
- ],
- \"metadata\": \"\",
- \"deposit\": \"10000000uside\",
- \"title\": \"Initial dkg for oracle\",
- \"summary\": \"Initiate DKG for oracle\",
- \"expedited\": false
-}"
+if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
-echo $ORACLE_PROPOSAL > ../build/oracle.json
-cat ../build/oracle.json
+  declare -a TSS_PARTICIPANTS
+  for i in $(seq 1 $N); do
+      key=$(jq .pub_key.value $HOME/.shuttler$i/priv_validator_key.json)
+      TSS_PARTICIPANTS+=($key)
+  done
 
-DCA_PROPOSAL="{
- \"messages\": [
- {
-  \"@type\": \"/side.dlc.MsgCreateAgency\",
-  \"authority\": \"side10d07y265gmmuvt4z0w9aw880jnsr700jwrwlg5\",
-  \"participants\": [ ${PARTICIPANTS%?} ],
-  \"threshold\": $T
- }
- ],
- \"metadata\": \"\",
- \"deposit\": \"10000000uside\",
- \"title\": \"Initial dkg for agency\",
- \"summary\": \"Initiate DKG for agency\",
- \"expedited\": false
-}"
+  PARTICIPANTS=$(printf "%s," ${TSS_PARTICIPANTS[@]})
 
-$BINARY tx gov submit-proposal ../build/oracle.json --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
-sleep 6
-echo $DCA_PROPOSAL > ../build/dca.json
-cat ../build/dca.json
-$BINARY tx gov submit-proposal ../build/dca.json --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
+  ORACLE_PROPOSAL="{
+  \"messages\": [
+  {
+    \"@type\": \"/side.dlc.MsgCreateOracle\",
+    \"authority\": \"side10d07y265gmmuvt4z0w9aw880jnsr700jwrwlg5\",
+    \"participants\": [ ${PARTICIPANTS%?} ],
+    \"threshold\": $T
+  }
+  ],
+  \"metadata\": \"\",
+  \"deposit\": \"10000000uside\",
+  \"title\": \"Initial dkg for oracle\",
+  \"summary\": \"Initiate DKG for oracle\",
+  \"expedited\": false
+  }"
 
-sleep 6
+  echo $ORACLE_PROPOSAL > ../build/oracle.json
+  cat ../build/oracle.json
 
-# Vote active proposals
-$BINARY q gov proposals --output json | jq -r '.proposals| .[] | select(.status == 2) | .id'| while read -r id; do 
-  echo "\n   >>>> Vote for $id"; 
-  $BINARY tx gov vote $id yes --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
+  DCA_PROPOSAL="{
+  \"messages\": [
+  {
+    \"@type\": \"/side.dlc.MsgCreateAgency\",
+    \"authority\": \"side10d07y265gmmuvt4z0w9aw880jnsr700jwrwlg5\",
+    \"participants\": [ ${PARTICIPANTS%?} ],
+    \"threshold\": $T
+  }
+  ],
+  \"metadata\": \"\",
+  \"deposit\": \"10000000uside\",
+  \"title\": \"Initial dkg for agency\",
+  \"summary\": \"Initiate DKG for agency\",
+  \"expedited\": false
+  }"
+
+  $BINARY tx gov submit-proposal ../build/oracle.json --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
   sleep 6
-done
+  echo $DCA_PROPOSAL > ../build/dca.json
+  cat ../build/dca.json
+  $BINARY tx gov submit-proposal ../build/dca.json --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
+
+  sleep 6
+
+  # Vote active proposals
+  $BINARY q gov proposals --output json | jq -r '.proposals| .[] | select(.status == 2) | .id'| while read -r id; do 
+    echo "\n   >>>> Vote for $id"; 
+    $BINARY tx gov vote $id yes --from validator --fees 1000uside --chain-id $CHAINID --keyring-backend $KEYRING -y
+    sleep 6;
+  done
+fi
 
 for i in $(seq 1 $N); do
   # RUST_BACKTRACE=1 
