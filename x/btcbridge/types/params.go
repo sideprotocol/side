d5d367e7dabd4ecd914752f20193188c6cc8bceb
keithsue <sufay28301@gmail.com>
2025-05-19 23:52:23 +0800

    modify bridge default params

--- x/btcbridge/types/params.go
+++ x/btcbridge/types/params.go
@@ -22,10 +22,10 @@ import (
 var (
 
 	// default confirmation depth for bitcoin deposit transactions
-	DefaultDepositConfirmationDepth = int32(6)
+	DefaultDepositConfirmationDepth = int32(1)
 
 	// default confirmation depth for bitcoin withdrawal transactions
-	DefaultWithdrawConfirmationDepth = int32(6)
+	DefaultWithdrawConfirmationDepth = int32(1)
 
 	// default BTC voucher denom
 	DefaultBtcVoucherDenom = "sat"
@@ -52,10 +52,10 @@ var (
 	DefaultIBCPortId = "transfer"
 
 	// default IBC timeout height offset
-	DefaultIBCTimeoutHeightOffset = uint64(1000)
+	DefaultIBCTimeoutHeightOffset = uint64(0)
 
 	// default IBC timeout duration
-	DefaultIBCTimeoutDuration = time.Duration(10) * time.Minute // 10 mins
+	DefaultIBCTimeoutDuration = time.Duration(86400) * time.Second // 1 day
 )
 
 // NewParams creates a new Params instance
@@ -327,7 +327,7 @@ func validateTSSParams(params *TSSParams) error {
 // validateIBCParams validates the given IBC params
 func validateIBCParams(params *IBCParams) error {
 	if err := ibchost.PortIdentifierValidator(params.PortId); err != nil {
-		return errorsmod.Wrapf(ErrInvalidParams, "invalid IBC port: %v", err)
+		return errorsmod.Wrapf(ErrInvalidParams, "invalid IBC port id: %v", err)
 	}
 
 	if params.TimeoutDuration < 0 {
