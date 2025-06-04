6d6c247b7ab446f76b32496b1b57dd14b823bba2
LazyLuis <ping@side.one>
2025-06-04 21:09:17 +0800

    fixed price/voting power

--- x/oracle/abci/extension.go
+++ x/oracle/abci/extension.go
@@ -351,6 +351,7 @@ func (h *PriceOracleVoteExtHandler) extractPricesAndBlockHeaders(_ sdk.Context, 
 	var totalStake int64
 
 	stakeWeightedPrices := make(map[string]math.LegacyDec, len(types.PRICE_CACHE)) // base -> average stake-weighted price
+	stakeWeightedVotingPower := make(map[string]int64, len(types.PRICE_CACHE))
 	blockHeaders := make(map[string][]*types.BlockHeader)
 	headerStakes := make(map[string]int64)
 
@@ -368,7 +369,7 @@ func (h *PriceOracleVoteExtHandler) extractPricesAndBlockHeaders(_ sdk.Context, 
 
 		h.logger.Warn("extension", "validator", hex.EncodeToString(v.Validator.Address), "extension", voteExt)
 
-		totalStake += v.Validator.Power
+		// totalStake += v.Validator.Power
 
 		// Compute stake-weighted average of prices for each supported pair, i.e.
 		// (P1)(W1) + (P2)(W2) + ... + (Pn)(Wn) / (W1 + W2 + ... + Wn)
@@ -388,8 +389,10 @@ func (h *PriceOracleVoteExtHandler) extractPricesAndBlockHeaders(_ sdk.Context, 
 			}
 			if _, ok := stakeWeightedPrices[base]; ok {
 				stakeWeightedPrices[base] = stakeWeightedPrices[base].Add(stakePrice.MulInt64(v.Validator.Power))
+				stakeWeightedVotingPower[base] += v.Validator.Power
 			} else {
 				stakeWeightedPrices[base] = stakePrice.MulInt64(v.Validator.Power)
+				stakeWeightedVotingPower[base] += v.Validator.Power
 			}
 		}
 
@@ -414,7 +417,9 @@ func (h *PriceOracleVoteExtHandler) extractPricesAndBlockHeaders(_ sdk.Context, 
 	// finalize average by dividing by total stake, i.e. total weights
 	for base, price := range stakeWeightedPrices {
 		if price.GT(math.LegacyZeroDec()) {
-			stakeWeightedPrices[base] = price.QuoInt64(totalStake)
+			if vp, ok := stakeWeightedVotingPower[base]; ok {
+				stakeWeightedPrices[base] = price.QuoInt64(vp)
+			}
 		} else {
 			h.logger.Error("Got invalid price.", "symbal", base, "price", price)
 		}
