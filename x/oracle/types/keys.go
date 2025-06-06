614e5c178313e8ceb3efceefaa353edc53708dd4
keithsue <sufay28301@gmail.com>
2025-06-06 13:24:27 +0800

    clean up

--- x/oracle/types/keys.go
+++ x/oracle/types/keys.go
@@ -35,10 +35,12 @@ var (
 	Percent  = math.NewInt(100)
 	Permille = math.NewInt(1000)
 
-	PriceKeyPrefix            = []byte{0x07}
-	BitcoinHeaderPrefix       = []byte{0x10}
-	BitcoinHeaderHeightPrefix = []byte{0x11} // prefix for each key to a block hash, for a height
-	BitcoinBestBlockHeaderKey = []byte{0x12} // key for the best block height
+	ParamsStoreKey = []byte{0x01}
+
+	PriceKeyPrefix            = []byte{0x10}
+	BitcoinHeaderPrefix       = []byte{0x11}
+	BitcoinHeaderHeightPrefix = []byte{0x12} // prefix for each key to a block hash, for a height
+	BitcoinBestBlockHeaderKey = []byte{0x13} // key for the best block height
 
 	PRICE_CACHE    = make(map[string]map[string]Price) // symbol, exchange, price[]
 	PriceMu        sync.RWMutex
