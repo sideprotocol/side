77b5bd153c270ffa0a8adc9601b97d67213810aa
keithsue <sufay28301@gmail.com>
2025-06-16 11:55:24 +0800

    relax check on redeeming collateral

--- x/lending/keeper/msg_server_loan.go
+++ x/lending/keeper/msg_server_loan.go
@@ -343,8 +343,9 @@ func (m msgServer) Redeem(goCtx context.Context, msg *types.MsgRedeem) (*types.M
 		return nil, types.ErrMismatchedBorrower
 	}
 
-	if loan.Status != types.LoanStatus_Rejected {
-		return nil, errorsmod.Wrap(types.ErrInvalidLoanStatus, "loan not rejected")
+	// check if the collateral is redeemable
+	if !types.CollateralRedeemable(loan) {
+		return nil, errorsmod.Wrap(types.ErrInvalidLoanStatus, "loan collateral not redeemable")
 	}
 
 	p, _ := psbt.NewFromRawBytes(bytes.NewReader([]byte(msg.Tx)), true)
