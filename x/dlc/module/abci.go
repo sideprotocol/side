d57e46c053bbcb10a62d88a58c716d634e86b0ff
keithsue <sufay28301@gmail.com>
2025-07-04 17:50:47 +0800

    optimize oracle participant liveness logic

--- x/dlc/module/abci.go
+++ x/dlc/module/abci.go
@@ -19,23 +19,19 @@ func generateLendingEventNonces(ctx sdk.Context, k keeper.Keeper) {
 		return
 	}
 
-	// check if there exist oracle participant base set
-	if len(k.OracleParticipantBaseSet(ctx)) == 0 {
-		return
-	}
-
 	// check if lending event nonces need to be generated
 	pendingLendingEventCount := k.GetPendingLendingEventCount(ctx)
 	if pendingLendingEventCount >= uint64(k.NonceQueueSize(ctx)) {
 		return
 	}
 
-	// get oracle participants
-	participants := k.GetOracleParticipants(ctx)
-	if len(participants) == 0 {
-		k.Logger(ctx).Info("No sufficient oracle participants", "required oracle participant num", k.OracleParticipantNum(ctx), "alive oracle participant num", len(k.GetAliveOracleParticipants(ctx)))
+	// check if there exist oracle participant base set
+	if len(k.OracleParticipantBaseSet(ctx)) == 0 {
 		return
 	}
+
+	// get oracle participants
+	participants := k.GetOracleParticipants(ctx)
 
 	// initiate DKG
 	k.TSSKeeper().InitiateDKG(ctx, types.ModuleName, types.DKG_TYPE_NONCE, int32(types.DKGIntent_DKG_INTENT_LENDING_EVENT_NONCE), participants, k.OracleParticipantThreshold(ctx), k.NonceGenerationBatchSize(ctx))
