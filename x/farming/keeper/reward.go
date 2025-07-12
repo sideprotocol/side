a07a186ff7f1301fa5650abe0d8830d41a5b572e
keithsue <sufay28301@gmail.com>
2025-07-13 02:24:54 +0800

    distribute reward per epoch

--- x/farming/keeper/reward.go
+++ x/farming/keeper/reward.go
@@ -4,15 +4,15 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// GetPendingReward gets the pending reward of the given staking
+// GetPendingReward gets the pending reward of the given staking for the current epoch
 // Assume that the given staking is valid
 func (k Keeper) GetPendingReward(ctx sdk.Context, stakingId uint64) sdk.Coin {
 	staking := k.GetStaking(ctx, stakingId)
 	totalStaking := k.GetTotalStaking(ctx, staking.Amount.Denom)
 
-	totalRewardsForAsset := k.RewardsPerEpoch(ctx).Amount.ToLegacyDec().Mul(k.GetAsset(ctx, staking.Amount.Denom).RewardRatio).TruncateInt()
+	totalRewards := k.RewardsPerEpoch(ctx).Amount.ToLegacyDec().Mul(k.GetAsset(ctx, staking.Amount.Denom).RewardRatio).TruncateInt()
 
-	rewardAmount := totalRewardsForAsset.Mul(staking.EffectiveAmount.Amount).Quo(totalStaking.EffectiveAmount.Amount)
+	rewardAmount := totalRewards.Mul(staking.EffectiveAmount.Amount).Quo(totalStaking.EffectiveAmount.Amount)
 
-	return sdk.NewCoin(staking.Amount.Denom, rewardAmount)
+	return sdk.NewCoin(k.RewardsPerEpoch(ctx).Denom, rewardAmount)
 }
