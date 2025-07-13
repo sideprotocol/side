753b78a60ae6741860aad98fd404520cc1ec0871
keithsue <sufay28301@gmail.com>
2025-07-13 19:35:16 +0800

    optimize farming

--- x/farming/module/genesis.go
+++ x/farming/module/genesis.go
@@ -12,14 +12,14 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
 
-	// create a new epoch if farming enabled
-	if genState.Params.Enabled {
-		k.NewEpoch(ctx)
-	}
-
 	// set stakings
 	for _, staking := range genState.Stakings {
 		k.SetStaking(ctx, staking)
+	}
+
+	// start the new epoch if farming enabled
+	if genState.Params.Enabled {
+		k.NewEpoch(ctx)
 	}
 }
 
