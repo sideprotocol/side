f1f4305d91315aaedc1953a3a48ea7ee6e369dcf
keithsue <sufay28301@gmail.com>
2025-07-17 13:46:30 +0800

    set module account in genesis

--- x/farming/module/genesis.go
+++ x/farming/module/genesis.go
@@ -1,6 +1,8 @@
 package farming
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sideprotocol/side/x/farming/keeper"
@@ -20,6 +22,18 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 	// start the new epoch if farming enabled
 	if genState.Params.Enabled {
 		k.NewEpoch(ctx)
+	}
+
+	// check if the module account exists
+	moduleAcc := k.GetModuleAccount(ctx)
+	if moduleAcc == nil {
+		panic(fmt.Sprintf("%s module account has not been set", types.ModuleName))
+	}
+
+	// set the module account if there is zero balance
+	balances := k.BankKeeper().GetAllBalances(ctx, moduleAcc.GetAddress())
+	if balances.IsZero() {
+		k.AuthKeeper().SetModuleAccount(ctx, moduleAcc)
 	}
 }
 
